d274ac0ac7efda927a8709735c1c4785f721aae6
KimYeoongBean <qhrtjs0430@naver.com>
2025-10-14 00:27:23 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<wireless_charger_simulrator>/', // 이 부분을 추가하세요!
+  base: '/wireless_charger_simulrator/', // 이 부분을 추가하세요!
 })
